c1a0a078c0b7755a7713323f5a7be410f0af74e2
voxel!() <voxelmc@hotmail.com>
2025-03-15 11:13:19 -0700

    Remove Moody and fix lualine

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -875,58 +875,58 @@ require("lazy").setup({
     --     end
     -- },
     -- {
-    "svampkorg/moody.nvim",
-    enabled = true,
-    event = { "ModeChanged", "BufWinEnter", "WinEnter" },
-    opts = {
-        -- you can set different blend values for your different modes.
-        -- Some colours might look better more dark, so set a higher value
-        -- will result in a darker shade.
-        blends = {
-            normal = 0.2,
-            insert = 0.2,
-            visual = 0.25,
-            command = 0.2,
-            operator = 0.2,
-            replace = 0.2,
-            select = 0.2,
-            terminal = 0.2,
-            terminal_n = 0.2,
-        },
-        -- there are two ways to define colours for the different modes.
-        -- one way is to define theme here in colors. Another way is to
-        -- set them up with highlight groups. Any highlight group set takes
-        -- precedence over any colours defined here.
-        colors = {
-            normal = "#00BFFF",
-            insert = "#70CF67",
-            visual = "#AD6FF7",
-            command = "#EB788B",
-            operator = "#FF8F40",
-            replace = "#E66767",
-            select = "#AD6FF7",
-            terminal = "#4CD4BD",
-            terminal_n = "#00BBCC",
-        },
-        -- disable filetypes here. Add for example "TelescopePrompt" to
-        -- not have any coloured cursorline for the telescope prompt.
-        disabled_filetypes = { "TelescopePrompt" },
-        -- you can turn on or off bold characters for the line numbers
-        bold_nr = true,
-        -- you can turn on and off a feature which shows a little icon and
-        -- registry number at the end of the CursorLine, for when you are
-        -- recording a macro! Default is false.
-        recording = {
-            enabled = true,
-            -- icon = "󰑋",
-            icon = "q",
-            -- you can set some text to surround the recording registry char with
-            -- or just set one to empty to maybe have just one letter, an arrow
-            -- perhaps! For example recording to q, you could have! "󰑋    q" :D
-            pre_registry_text = "[",
-            post_registry_text = "]",
-        },
-    },
+    -- "svampkorg/moody.nvim",
+    -- enabled = true,
+    -- event = { "ModeChanged", "BufWinEnter", "WinEnter" },
+    -- opts = {
+    --     -- you can set different blend values for your different modes.
+    --     -- Some colours might look better more dark, so set a higher value
+    --     -- will result in a darker shade.
+    --     blends = {
+    --         normal = 0.2,
+    --         insert = 0.2,
+    --         visual = 0.25,
+    --         command = 0.2,
+    --         operator = 0.2,
+    --         replace = 0.2,
+    --         select = 0.2,
+    --         terminal = 0.2,
+    --         terminal_n = 0.2,
+    --     },
+    --     -- there are two ways to define colours for the different modes.
+    --     -- one way is to define theme here in colors. Another way is to
+    --     -- set them up with highlight groups. Any highlight group set takes
+    --     -- precedence over any colours defined here.
+    --     colors = {
+    --         normal = "#00BFFF",
+    --         insert = "#70CF67",
+    --         visual = "#AD6FF7",
+    --         command = "#EB788B",
+    --         operator = "#FF8F40",
+    --         replace = "#E66767",
+    --         select = "#AD6FF7",
+    --         terminal = "#4CD4BD",
+    --         terminal_n = "#00BBCC",
+    --     },
+    --     -- disable filetypes here. Add for example "TelescopePrompt" to
+    --     -- not have any coloured cursorline for the telescope prompt.
+    --     disabled_filetypes = { "TelescopePrompt" },
+    --     -- you can turn on or off bold characters for the line numbers
+    --     bold_nr = true,
+    --     -- you can turn on and off a feature which shows a little icon and
+    --     -- registry number at the end of the CursorLine, for when you are
+    --     -- recording a macro! Default is false.
+    --     recording = {
+    --         enabled = true,
+    --         -- icon = "󰑋",
+    --         icon = "q",
+    --         -- you can set some text to surround the recording registry char with
+    --         -- or just set one to empty to maybe have just one letter, an arrow
+    --         -- perhaps! For example recording to q, you could have! "󰑋    q" :D
+    --         pre_registry_text = "[",
+    --         post_registry_text = "]",
+    --     },
+    -- },
 
     -- Error list
     {
@@ -1162,7 +1162,7 @@ require("lazy").setup({
         lazy = false,
         config = function()
             local ayucolors = require('ayu.colors')
-            ayucolors.generate()
+            ayucolors.generate(false)
 
             require('ayu').setup({
                 overrides = {
@@ -1332,11 +1332,11 @@ require("lazy").setup({
         -- See `:help lualine.txt`
         opts = {
             options = {
-                icons_enabled = false,
+                icons_enabled = true,
                 -- theme = "tokyonight",
                 -- theme = "ayu",
                 -- theme = "ayu-light",
-                theme = "onedark",
+                theme = "everforest",
                 component_separators = "|",
                 section_separators = "",
             },
@@ -1993,7 +1993,7 @@ vim.keymap.set("n", "<C-?>", function()
         { title = "File Diagnostics", render = "simple", timeout = diagTimeout, hide_from_history = true, animate = false })
 end, { desc = "Get all diagnostics in current file" })
 
-require 'lualine'.setup { options = { theme = 'everforest' } }
+-- require 'lualine'.setup { options = { theme = 'everforest' } }
 
 -- require("settings.theme")
 
